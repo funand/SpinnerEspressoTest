5f139e3c30c1dd3c29c1ac2dfa6beb64363e42b1
Prince <davidikede24@gmail.com>
2018-10-23 21:06:27 -0400

    Removed some comments

--- app/src/androidTest/java/com/example/android/phonenumberspinner/SpinnerSelectionTest.java
+++ app/src/androidTest/java/com/example/android/phonenumberspinner/SpinnerSelectionTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.android.phonenumberspinner;
 
@@ -73,22 +58,5 @@ public class SpinnerSelectionTest {
             onView(withId(R.id.text_phonelabel)).check(matches(withText(containsString(myArray[i]))));
 
         }
-
-
-
-//        String[] myArray = mActivityRule.getActivity().getResources()
-//                .getStringArray(R.array.labels_array);
-//        // Iterate through the spinner array of items.
-//        int size = myArray.length;
-//        for (int i=0; i<size; i++) {
-//            // Find the spinner and click on it.
-//            onView(withId(R.id.label_spinner)).perform(click());
-//            // Find the spinner item and click on it.
-//            onData(is(myArray[i])).perform(click());
-//            // Find the text view and check that the spinner item
-//            // is part of the string.
-//            onView(withId(R.id.text_phonelabel))
-//                    .check(matches(withText(containsString(myArray[i]))));
-
     }
 }
